b18edcbdb0bd89200edcd710291ce96eb8584494
afsalpc <afsalparachodan@outlook.com>
2024-02-15 19:36:22 +0530

    created afsa

---
+++ src/main/java/com/example/afsa/AfsaApplication.java
@@ -0,0 +1,12 @@
+package com.example.afsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfsaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/afsa/AfsaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.afsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfsaApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
